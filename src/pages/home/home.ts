45bd7aacb690fef93f01bcada087b3332bfdff8d
Nam Nguyen <vunam289@gmail.com>
2017-06-29 10:06:14 +0700

    add resource

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,62 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public contactList: any;
 
   constructor(public navCtrl: NavController) {
+    this.contactList = [{
+      id: 11,
+      displayName: "Nam 1",
 
+    }, {
+      id: 12,
+      displayName: "Nam 2"
+    }, {
+      id: 13,
+      displayName: "Nam 3"
+    }, {
+      id: 14,
+      displayName: "Nam 4"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }, {
+      id: 15,
+      displayName: "Nam 5"
+    }];
   }
 
 }
